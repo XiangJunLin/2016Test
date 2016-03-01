8b22f47936264dfce17165376517076ed8d36df0
LiuZHao <comst@comstdemacbook-pro.local>
2016-03-01 15:46:09 +0800

    init

---
+++ LZParentChildPro/LZParentChildPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZParentChildPro
+//
+//  Created by comst on 16/3/1.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
