1b20ccaa2695e88408d01cb0bbae182b462f3986
LiuZHao <comst@comstdemacbook-pro.local>
2016-03-02 06:02:34 +0800

    init

---
+++ LZCollectionViewPro/LZCollectionViewPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZCollectionViewPro
+//
+//  Created by comst on 16/3/2.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LZCollectionViewPro/LZCollectionViewPro/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LZCollectionViewPro
+//
+//  Created by comst on 16/3/2.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
