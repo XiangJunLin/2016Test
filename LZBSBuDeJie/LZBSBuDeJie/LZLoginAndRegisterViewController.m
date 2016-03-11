c2ee982489e7a595c059ef3d1822205ebf49a720
LiuZHao <comst@comstdemacbook-pro.local>
2016-03-11 14:35:17 +0800

    login register

--- LZBSBuDeJie/LZBSBuDeJie/LZLoginAndRegisterViewController.m
+++ LZBSBuDeJie/LZBSBuDeJie/LZLoginAndRegisterViewController.m
@@ -9,6 +9,7 @@
 #import "LZLoginAndRegisterViewController.h"
 
 @interface LZLoginAndRegisterViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginLeftConstraint;
 
 @end
 
@@ -23,9 +24,32 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 - (IBAction)btnClose:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+
+- (IBAction)registerClick:(id)sender {
+    UIButton *btn = (UIButton *)sender;
+    if (self.loginLeftConstraint.constant == 0) {
+        self.loginLeftConstraint.constant = - screenWidth ;
+        [UIView animateWithDuration:0.25 animations:^{
+            [self.view layoutIfNeeded];
+        }];
+        [btn setTitle:@"已有账号" forState:UIControlStateNormal];
+    }else{
+        self.loginLeftConstraint.constant = 0;
+        [UIView animateWithDuration:0.25 animations:^{
+            [self.view layoutIfNeeded];
+        }];
+        [btn setTitle:@"快速注册" forState:UIControlStateNormal];
+    }
+}
+
+
+
 
 - (UIStatusBarStyle)preferredStatusBarStyle{
     return UIStatusBarStyleLightContent;
